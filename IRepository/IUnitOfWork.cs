ef5643ed86bed14c4a953497badb6814109cf15a
Timothy Karani <kartimothy@gmail.com>
2021-07-18 17:30:07 +0300

    Added unitofwork

---
+++ IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using HotelListing.Data;
+
+namespace HotelListing.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+
+        Task Save();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.Data;
+using HotelListing.IRepository;
+
+namespace HotelListing.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+        // private readonly DbSet<T> _db;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context);
+        public IGenericRepository<Hotel> Hotels => _hotels ??= new GenericRepository<Hotel>(_context);
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
