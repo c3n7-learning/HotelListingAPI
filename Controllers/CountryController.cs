9bc676d68db875279a224f87c9e268f927813d9c
Timothy Karani <kartimothy@gmail.com>
2022-05-14 23:37:50 +0300

    Getting countries

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HotelListing.IRepository;
+
+namespace HotelListing.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : ControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                return Ok(countries);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(GetCountries)}");
+                return StatusCode(500, "Internal Server Error. Please Try Again Later.");
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,9 @@ using HotelListing.Data;
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using HotelListing.Configurations;
+using HotelListing.IRepository;
+using HotelListing.Repository;
+using Newtonsoft;
 
 namespace HotelListing
 {
@@ -45,12 +48,15 @@ namespace HotelListing
 
             services.AddAutoMapper(typeof(MapperInitializer));
 
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op =>
+             op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
