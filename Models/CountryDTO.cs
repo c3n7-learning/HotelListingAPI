da7b76122262a760c992a43baf9fd9a286b99d42
Timothy Karani <kartimothy@gmail.com>
2022-05-14 22:49:38 +0300

    Added binding

--- Models/CountryDTO.cs
+++ Models/CountryDTO.cs
@@ -1,21 +1,8 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
 
 namespace HotelListing.Models
 {
-    public class CountryDTO
-    {
-        public int Id { get; set; }  // prop
-
-        [Required]
-        [StringLength(maximumLength: 50, ErrorMessage = "Country Name is Too Long")]
-        public string Name { get; set; }
-
-        [Required]
-        [StringLength(maximumLength: 3, ErrorMessage = "Short Name is Too Long")]
-        public string ShortName { get; set; }
-    }
-
     public class CreateCountryDTO
     {
         [Required]
@@ -27,5 +14,11 @@ namespace HotelListing.Models
         public string ShortName { get; set; }
     }
 
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }  // prop
+
+        public IList<HotelDTO> Hotels { get; set; }
+    }
 }
 
